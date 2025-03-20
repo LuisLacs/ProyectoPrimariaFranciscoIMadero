74793f3860313fd76ae057ea323d909230953b70
LuisLacs <luislacsgamer@gmail.com>
2025-03-19 21:01:58 -0600

    Actualiazación

--- ProyectoEscuela/Login.cs
+++ ProyectoEscuela/Login.cs
@@ -24,12 +24,29 @@ namespace ProyectoEscuela
 
         private void guna2Button1_Click(object sender, EventArgs e)
         {
-            if (tbUser.Text != string.Empty || tbPass.Text != string.Empty)
+            try
             {
-                DirectorIndex director = new DirectorIndex();
-                director.ShowDialog();
-                this.Close();
+                if (tbUser.Text != string.Empty || tbPass.Text != string.Empty)
+                {
+                    DirectorIndex director = new DirectorIndex();
+                    //Aqui va la conexion
+                    director.ShowDialog();
+                    this.Close();
+                }
+                else if (tbUser.Text == string.Empty)
+                {
+                    MessageBox.Show("Favor ingresar un usuario", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (tbPass.Text == string.Empty)
+                {
+                    MessageBox.Show("Favor ingresar una contraseña", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
             }
+            catch 
+            {
+                MessageBox.Show("Contraseña Incorrecta");
+            }
+            
         }
 
         private void guna2Button2_Click(object sender, EventArgs e)
